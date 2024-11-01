96a45607aa071b30ff9df5a4295081497529bdf0
Ariel Lira <ariellira.al@gmail.com>
2024-11-01 14:05:39 -0700

    formatting

--- src/core/cpu.cpp
+++ src/core/cpu.cpp
@@ -553,9 +553,7 @@ Cpu::Cpu()
           std::bind(&Cpu::set_7_a, this),
     } {}
 
-const Registers& Cpu::registers() const {
-    return registers_;
-}
+const Registers &Cpu::registers() const { return registers_; }
 
 uint8_t Cpu::fetch_current_instruction() {
   uint8_t opcode = memory_.read_byte(registers_.pc);
